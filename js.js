68864510c76f8acda5dcb6e7480fbfc1e9a57f55
Curtis Myers <curtismyersprogramming@gmail.com>
2021-11-17 16:44:15 +0000

    some js added need to add rest of functionality and change some css

--- js.js
+++ js.js
@@ -1,3 +1,52 @@
+let userScore = 0;
+let compScore = 0;
+const userScore_Span = document.getElementById("user-score");
+const compScore_Span = document.getElementById("computer-score");
+const rock_button = document.getElementById("rock-button");
+const paper_button = document.getElementById("paper-button");
+const scissors_button = document.getElementById("scissors-button");
+const result = document.querySelector(".result > p");
+
+
+//event listeners//
+
+    //Buttons//
+    rock_button.addEventListener('click',function(){
+        playRound("Rock",computerPlay());
+    })
+
+    paper_button.addEventListener('click',function(){
+        playRound("Paper",computerPlay());
+    })
+
+    scissors_button.addEventListener('click',function(){
+        playRound("Scissors",computerPlay());
+    })
+
+
+
+
+
+
+// game functionality //
+
+    //game outcomes
+
+    function  win(playerSelection, computerSelection){
+        userScore++;
+        userScore_Span.innerHTML = userScore;
+        result.innerHTML =  playerSelection+ " beats " + computerSelection + "! You WIN!"
+    }
+
+    function  lose(){
+        console.log("lose");
+    }
+
+    function  draw(){
+        console.log("draw");
+    }
+
+
 function computerPlay(){
     let options = ["Rock", "Paper", "Scissors"];
     let selction = Math.floor(Math.random()*3);
@@ -5,21 +54,22 @@ function computerPlay(){
 }
 
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection){
+    const computerSelection = computerPlay();
   
-        if(playerSelection.toLowerCase() == "paper" && computerSelection == "Paper" || playerSelection.toLowerCase() == "rock" && computerSelection == "Rock" || playerSelection.toLowerCase() == "scissors" && computerSelection == "Scissors") {
-            return("draw");
-        } else if(playerSelection.toLowerCase() == "paper" && computerSelection == "Rock" || playerSelection.toLowerCase() == "scissors" && computerSelection == "Paper" || playerSelection.toLowerCase() == "rock" && computerSelection == "Scissors"){
-           
-            return("win");
+        if(playerSelection == "Paper" && computerSelection == "Paper" || playerSelection == "Rock" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Scissors") {
+            draw();
+        } else if(playerSelection== "Paper" && computerSelection == "Rock" || playerSelection== "Scissors" && computerSelection == "Paper" || playerSelection== "Rock" && computerSelection == "Scissors"){
+           win(playerSelection, computerSelection);
+            // return("win");
         } else {
-            return("loss");
+           lose();
+            // return("loss");
         }
 }
 
 function game(){
-    let playerScore = 0;
-    let compScore = 0;
+   
     
     for(i =0; i<=4; i++){
         let playerSelection = prompt("rock paper scissors");
@@ -48,9 +98,6 @@ function game(){
     
 
 }
-
-
-
 
 
 
