803399d7e5c75f2d23ecda5f8123779afa5fd3cb
Curtis Myers <curtismyersprogramming@gmail.com>
2021-11-18 01:21:26 +0000

    v2 push README contains details on what the latest version contains and what must be changed

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # RockPaperScissorsJS
 A javascript based game of rock paper scissors 
 
+
+## V2 - 17/11/21
+This latest version of the game now has improved css and includes javaScript to manipulate HTML elemets on the page.
+
+Functions to be added:
+Button that appears to restart the game once either user reaches 5 points.
+fix the button selection by either replacing the current ones or adjusting them.
+
 ## V1 - 13/11/21
 First version of the game is a simple console based version runs with the function call game();
--- js.js
+++ js.js
@@ -1,16 +1,23 @@
-let userScore = 0;
-let compScore = 0;
-const userScore_Span = document.getElementById("user-score");
-const compScore_Span = document.getElementById("computer-score");
-const rock_button = document.getElementById("rock-button");
-const paper_button = document.getElementById("paper-button");
-const scissors_button = document.getElementById("scissors-button");
-const result = document.querySelector(".result > p");
+// VARIABLES //
+
+//scores that are kept track of during the game//
+let userScore = 0; //players score
+let compScore = 0; //computers score
+
+//HTML Variables//
+const userScore_Span = document.getElementById("user-score"); // user score on scoreboard
+const compScore_Span = document.getElementById("computer-score");// computer score on scoreboard
+const rock_button = document.getElementById("rock-button"); //button for rock
+const paper_button = document.getElementById("paper-button"); //button for paper
+const scissors_button = document.getElementById("scissors-button"); //button for scissors
+const result = document.querySelector(".result > p"); //div that holds the result text eg "rock beats paper"
 
 
 //event listeners//
 
     //Buttons//
+
+    //Each button has an event listener attched that when the button is clicked the correct response is passed on through a function
     rock_button.addEventListener('click',function(){
         playRound("Rock",computerPlay());
     })
@@ -32,40 +39,55 @@ const result = document.querySelector(".result > p");
 
     //game outcomes
 
-    function  win(playerSelection, computerSelection){
+    //Each function here produces an outcome corresponding to its name
+    function  win(playerSelection, computerSelection){ // Adds a single win point onto the usersScore and updates the scoreboard
         userScore++;
         userScore_Span.innerHTML = userScore;
         result.innerHTML =  playerSelection+ " beats " + computerSelection + "! You WIN!"
+        resultCheck();
+        
     }
 
-    function  lose(){
-        console.log("lose");
+    function  lose(playerSelection, computerSelection){ // Adds a single win point onto the compScore and updates the scoreboard
+        compScore++;
+        compScore_Span.innerHTML = compScore;
+        result.innerHTML =  computerSelection+ " beats " + playerSelection + "! You LOSE!"
+        resultCheck();
     }
 
-    function  draw(){
-        console.log("draw");
+    function  draw(playerSelection){ // no points added onto the scoreboard and message shown to let user know the result was a draw
+        result.innerHTML =  "You both selected " + playerSelection + " so its a draw" 
+        resultCheck();
     }
 
-
+    //Computers choice function
 function computerPlay(){
     let options = ["Rock", "Paper", "Scissors"];
     let selction = Math.floor(Math.random()*3);
     return options[selction];
 }
 
-
+    //players choice function
+    //takes the user seelction from a button press then compares that selection
+    //against all possible outcomes and calls the correct function based on that
 function playRound(playerSelection){
     const computerSelection = computerPlay();
   
         if(playerSelection == "Paper" && computerSelection == "Paper" || playerSelection == "Rock" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Scissors") {
-            draw();
+            draw(playerSelection);
         } else if(playerSelection== "Paper" && computerSelection == "Rock" || playerSelection== "Scissors" && computerSelection == "Paper" || playerSelection== "Rock" && computerSelection == "Scissors"){
            win(playerSelection, computerSelection);
-            // return("win");
         } else {
-           lose();
-            // return("loss");
+           lose(playerSelection, computerSelection);
         }
+}
+
+function resultCheck(){
+    if(userScore > 4 ){
+        result.innerHTML = "YOU WON THE GAME!"
+    } else if (compScore > 4){
+        result.innerHTML = "YOU LOST THE GAME :("
+    }
 }
 
 function game(){
